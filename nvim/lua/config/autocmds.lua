e610f2d8254ce31c93da51f1f63e5531aac8d2a0
eeee0717 <chentao020717Work@outlook.com>
2025-01-21 10:51:05 +0800

    chore

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,21 +1,3 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
-
--- 确保在LSP 配置完成之前关闭virtual_text
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function()
-    vim.diagnostic.config({
-      virtual_text = false, -- Disable virtual text
-    })
-  end,
-})
 local function augroup(name)
   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
