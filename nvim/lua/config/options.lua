21b0d8fb810156c37b3d3369c0e04e584275e68e
eeee0717 <chentao020717Work@outlook.com>
2025-01-25 15:06:42 +0800

    chore: add ufo

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -12,7 +12,9 @@ opt.list = true
 opt.listchars = { space = "·" }
 opt.scrolloff = 5
 opt.indentexpr = ""
-opt.foldmethod = "indent"
-opt.foldlevel = 99
-opt.foldenable = true
-opt.foldlevelstart = 99
+
+-- ufo
+vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
